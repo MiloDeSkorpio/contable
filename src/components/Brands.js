7e39b0da2a809729fb6606c31b7f8c28a0371e02
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-04-05 05:49:42 -0500

    Products

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -6,7 +6,7 @@ const Brands = () => {
   //destructure overwiew
   const {brands} = overview
   return (
-    <section className='py-9'>
+    <section className='py-9 bg-orange-100'>
       <div className='container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6'>
         {/**  brands **/}
         {brands.map((item,index)=> {
