d71ac9aeaa76d965d120aec80cfe6cf651a4d1ea
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-04-05 18:04:23 -0500

    Footer

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,7 +1,40 @@
 import React from 'react';
+//import data
+import { copyright } from '../data';
 
 const Copyright = () => {
-  return <div>Copyright</div>;
+  //destructure copyrigth data
+  const {link1, link2, copyText, social } = copyright
+  return (
+      <div>
+        {/** links **/}
+        <div 
+          className='flex flex-col items-center gap-y-2 lg:flex-row justify-between' 
+          data-aos='fade-up'
+          data-aos-offset='0'
+          data-aos-delay='200'
+        >
+          <a className='hover:text-accent transition' href={link1.href}>{link1.name}</a>
+          <a className='hover:text-accent transition' href={link2.href}>{link2.name}</a>
+        </div>
+        {/** copyright **/}
+        <div>{copyText}</div>
+        {/** social icons **/}
+        <ul className='flex gap-x-[12px]'>
+          {social.map((item,index) => {
+            //destrucuture icon
+            const { href, icon } = item
+            return (
+              <li key={index}>
+                <a href={href}>
+                  <img src={icon} alt='social'/>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
 };
 
 export default Copyright;
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -18,9 +18,7 @@ const MobileNav = () => {
           </li>
           )
         })}
-
       </ul>
-      
     </div>
   )
 };
