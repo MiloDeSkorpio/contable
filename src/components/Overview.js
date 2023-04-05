a94ddd54b78c9e44bf086b68e3e1f1778a80e33f
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-04-05 04:33:04 -0500

    Overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
+//import data
+import { overview  } from '../data';
 const Overview = () => {
-  return <div>Overview</div>;
+  //destrucutre overview data
+  const {productImg } = overview
+  return (
+      <section className='lg:min-h-[712px] bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
+        <div className='container mx-auto flex justify-end overflow-hidden'>
+          <img 
+            src={productImg}
+            alt='product Img'
+            data-aos='fade-up'
+            data-aos-delay='300'
+          /> 
+
+        </div>
+      </section>
+    )
 };
 
 export default Overview;
